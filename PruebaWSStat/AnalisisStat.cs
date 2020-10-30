c7d44c6615809110a6c22d3b8ef4d6c77073f7b6
Alan Michel Solorzano Tapia <K697344@nac.ppg.com>
2020-10-30 13:26:13 -0600

    Agregar archivos de proyecto.

---
+++ PruebaWSStat/AnalisisStat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaWSStat
+{
+    public class AnalisisStat
+    {
+        public string usuario { get; set; }
+        public int contador { get; set; }
+        public decimal promedio { get; set; }
+    }
+}
---
+++ PruebaWSStat/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaWSStat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.SUtiFolioStat_PortTypeClient stat = new ServiceReference1.SUtiFolioStat_PortTypeClient();
+
+            ServiceReference1.DBA_ConsultarStatsInput input = new ServiceReference1.DBA_ConsultarStatsInput();
+
+
+
+            ServiceReference1.consultarFoliosStatRequest request = new ServiceReference1.consultarFoliosStatRequest();
+
+
+            ServiceReference1.DBA_ConsultarStatsOutput[] resultado = stat.consultarFoliosStat(input);
+
+
+
+
+            WebReference.SUtiFolioStatBindingQSService service = new WebReference.SUtiFolioStatBindingQSService();
+
+            WebReference.DBA_ConsultarStatsInput inn = new WebReference.DBA_ConsultarStatsInput();
+
+            WebReference.DBA_ConsultarStatsOutput[] res = service.consultarFoliosStat(inn);
+
+            List<string> listaUsuario = res.Select(n => n.assigned_user).Distinct().ToList();
+
+            List<AnalisisStat> listaAnalisisStat = new List<AnalisisStat>();
+
+
+            listaUsuario.ForEach(n =>
+            {
+                AnalisisStat analisisStat = new AnalisisStat();
+
+                analisisStat.usuario = n;
+                analisisStat.contador = res.Where(m => m.assigned_user == n).Count();
+
+                listaAnalisisStat.Add(analisisStat);
+
+            });
+
+            listaAnalisisStat = listaAnalisisStat.OrderByDescending(n => n.contador).ToList();
+
+        }
+    }
+}
